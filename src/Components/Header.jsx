4bdb64cc2e23f7c4986fb9f7410c0ac94491abdf
srseen <seereen.2540@gmail.com>
2024-12-10 22:31:43 +0700

    Fix error hamburger icon for responsive in Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,49 +1,21 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+
 const Header = () => {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div className="container">
-          <a className="navbar-brand" href="#section">
-            SRseeN
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="#section">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#myproject">
-                  My Project
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#skillbackend">
-                  Skill Backend
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#footer">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
+    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+      <Container>
+        <Navbar.Brand href="#section">SRseeN</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#section">Home</Nav.Link>
+            <Nav.Link href="#myproject">My Project</Nav.Link>
+            <Nav.Link href="#skillbackend">Skill Backend</Nav.Link>
+            <Nav.Link href="#footer">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 

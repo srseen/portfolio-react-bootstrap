cb4687400d466521e59858e0db5d8f7dd64e38d4
srseen <seereen.2540@gmail.com>
2024-12-10 20:41:09 +0700

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./Components/Header";
 import Section from "./Components/Section";
 import Skillbackend from "./Components/Skillbackend";
+import Footer from "./Components/Footer";
+
 function App() {
   return (
     <>
       <Header />
       <Section />
       <Skillbackend />
+      <Footer />
     </>
   );
 }

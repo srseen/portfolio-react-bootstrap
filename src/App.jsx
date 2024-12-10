fa7bad1cd6a2e6dcfc451c1a4b5a4b014e4c288e
srseen <seereen.2540@gmail.com>
2024-12-10 20:39:27 +0700

    Add Skillbackend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Components/Header";
 import Section from "./Components/Section";
+import Skillbackend from "./Components/Skillbackend";
 function App() {
   return (
     <>
       <Header />
       <Section />
+      <Skillbackend />
     </>
   );
 }
---
+++ src/Components/Skillbackend.jsx
@@ -0,0 +1,186 @@
+const Skillbackend = () => {
+  return (
+    <>
+      {" "}
+      <div className="container px-4 py-5" id="icon-grid">
+        <h2 className="pb-2 border-bottom">Backend Development Skills</h2>
+
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+          {/* Programming Languages */}
+          <div className="col d-flex align-items-start">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.75em"
+              height="1.75em"
+              fill="currentColor"
+              className="bi text-body-secondary flex-shrink-0 me-3"
+              viewBox="0 0 16 16"
+            >
+              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+              <path d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z" />
+            </svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">
+                Programming Languages
+              </h3>
+              <p>
+                Proficient in C#, Python, Java, and JavaScript for server-side
+                development.
+              </p>
+            </div>
+          </div>
+
+          {/* Databases */}
+          <div className="col d-flex align-items-start">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.75em"
+              height="1.75em"
+              fill="currentColor"
+              className="bi text-body-secondary flex-shrink-0 me-3"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3.904 1.777C4.978 1.289 6.427 1 8 1s3.022.289 4.096.777C13.125 2.245 14 2.993 14 4s-.875 1.755-1.904 2.223C11.022 6.711 9.573 7 8 7s-3.022-.289-4.096-.777C2.875 5.755 2 5.007 2 4s.875-1.755 1.904-2.223Z" />
+              <path d="M2 6.161V7c0 1.007.875 1.755 1.904 2.223C4.978 9.71 6.427 10 8 10s3.022-.289 4.096-.777C13.125 8.755 14 8.007 14 7v-.839c-.457.432-1.004.751-1.49.972C11.278 7.693 9.682 8 8 8s-3.278-.307-4.51-.867c-.486-.22-1.033-.54-1.49-.972Z" />
+              <path d="M2 9.161V10c0 1.007.875 1.755 1.904 2.223C4.978 12.711 6.427 13 8 13s3.022-.289 4.096-.777C13.125 11.755 14 11.007 14 10v-.839c-.457.432-1.004.751-1.49.972-1.232.56-2.828.867-4.51.867s-3.278-.307-4.51-.867c-.486-.22-1.033-.54-1.49-.972Z" />
+              <path d="M2 12.161V13c0 1.007.875 1.755 1.904 2.223C4.978 15.711 6.427 16 8 16s3.022-.289 4.096-.777C13.125 14.755 14 14.007 14 13v-.839c-.457.432-1.004.751-1.49.972-1.232.56-2.828.867-4.51.867s-3.278-.307-4.51-.867c-.486-.22-1.033-.54-1.49-.972Z" />
+            </svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">
+                Database Management
+              </h3>
+              <p>Experience with PostgreSQL and MongoDB.</p>
+            </div>
+          </div>
+
+          {/* API Development */}
+          <div className="col d-flex align-items-start">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.75em"
+              height="1.75em"
+              fill="currentColor"
+              className="bi text-body-secondary flex-shrink-0 me-3"
+              viewBox="0 0 16 16"
+            >
+              <path d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5v-1zM8.5 5a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1zM0 11.5A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm4.5.5A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm4.5.5a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z" />
+            </svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">
+                API Development
+              </h3>
+              <p>RESTful API design and API documentation with Swagger.</p>
+            </div>
+          </div>
+
+          {/* Web Frameworks */}
+          <div className="col d-flex align-items-start">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.75em"
+              height="1.75em"
+              fill="currentColor"
+              className="bi text-body-secondary flex-shrink-0 me-3"
+              viewBox="0 0 16 16"
+            >
+              <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
+            </svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">
+                Web Frameworks
+              </h3>
+              <p>Node.js/Express, Django, Spring Boot, and .Net Core.</p>
+            </div>
+          </div>
+
+          {/* Cloud Services */}
+          <div className="col d-flex align-items-start">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.75em"
+              height="1.75em"
+              fill="currentColor"
+              className="bi text-body-secondary flex-shrink-0 me-3"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z" />
+            </svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">
+                Cloud Services
+              </h3>
+              <p>
+                AWS, Google Cloud Platform, and Azure deployment experience.
+              </p>
+            </div>
+          </div>
+
+          {/* DevOps */}
+          <div className="col d-flex align-items-start">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.75em"
+              height="1.75em"
+              fill="currentColor"
+              className="bi text-body-secondary flex-shrink-0 me-3"
+              viewBox="0 0 16 16"
+            >
+              <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+            </svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">DevOps</h3>
+              <p>
+                Docker, Kubernetes, CI/CD pipelines, and Git version control.
+              </p>
+            </div>
+          </div>
+
+          {/* Security */}
+          <div className="col d-flex align-items-start">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.75em"
+              height="1.75em"
+              fill="currentColor"
+              className="bi text-body-secondary flex-shrink-0 me-3"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z" />
+            </svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Security</h3>
+              <p>
+                Authentication, authorization, data encryption, and security
+                best practices.
+              </p>
+            </div>
+          </div>
+
+          {/* Testing */}
+          <div className="col d-flex align-items-start">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.75em"
+              height="1.75em"
+              fill="currentColor"
+              className="bi text-body-secondary flex-shrink-0 me-3"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z" />
+              <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z" />
+            </svg>
+            <div>
+              <h3 className="fw-bold mb-0 fs-4 text-body-emphasis">Testing</h3>
+              <p>
+                Unit testing, integration testing, and test automation
+                frameworks.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Skillbackend;
